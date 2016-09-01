7e1d104fb0dfa7d32b96ca32dbb929f6dd388274
Petru Obreja <Petru.Obreja@ness.com>
2016-09-01 10:50:01 +0300

    Added support for documentation using Swagger. Added a README file for the project.

---
+++ README.md
@@ -0,0 +1,18 @@
+# Sync Registry Sample
+
+A project for a Sync Registry Sample
+
+### Build the service and running the JUnits and Integration Tests
+```sh
+mvn clean verify
+```
+
+### Starting the service
+(the service will start on localhost:1111, as configured in sync-registry-server.yml)
+```sh
+java -jar target/sync-registration-microservices-1.0-SNAPSHOT.jar
+```
+
+### REST API
+
+See the [API documentation](http://localhost:1111/swagger-ui.html)
--- src/main/java/org/petru/syncregistry/services/SyncRegitryServer.java
+++ src/main/java/org/petru/syncregistry/services/SyncRegitryServer.java
@@ -2,15 +2,38 @@ package org.petru.syncregistry.services;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @EnableAutoConfiguration
 @Import(SyncRegitryConfiguration.class)
+@EnableSwagger2
 public class SyncRegitryServer {
 
 	public static void main(String[] args) {
 		System.setProperty("spring.config.name", "sync-registry-server");
 
 		SpringApplication.run(SyncRegitryServer.class, args);
+	}
+
+	@Bean
+	public Docket swaggerSettings() {
+		return new Docket(DocumentationType.SWAGGER_2).apiInfo(apiInfo())
+				.select().apis(RequestHandlerSelectors.any())
+				.paths(PathSelectors.regex("(/syncregistry.*)")).build()
+				.pathMapping("/");
+	}
+
+	private ApiInfo apiInfo() {
+		return new ApiInfoBuilder().title("Sync Registry")
+				.description("Sync Registry Sample").build();
 	}
 }
